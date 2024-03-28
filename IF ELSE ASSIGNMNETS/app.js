5ab2191107a4e65419629a96c96690c4a8d34d88
ALYARA-MOBILE-ACCESSORIES <alyaramobileaccessories@gmail.com>
2024-03-29 00:15:02 +0500

    SECOND COMMIT

--- IF ELSE ASSIGNMNETS/app.js
+++ IF ELSE ASSIGNMNETS/app.js
@@ -1,2 +1,59 @@
 // question no 1 
 
+// let input = document.querySelector('#input')
+
+// function check(){
+//  let sum = Number(input.value) % 3
+
+//  if(sum === 0){
+//     console.log("number is divisible by 3");
+//  } else {
+//     console.log("number is not divisible by 3");
+//  }
+
+// }
+
+
+// question no 2 
+
+
+// let inputone= document.querySelector('#input-one')
+
+
+// function checkNumber(){
+
+// let sumone = Number(inputone.value) % 2;
+
+// if(sumone === 0){
+// console.log("CONGRATS NUMBER IS  EVEN");
+// } else {
+//     console.log("NUMBER IS ODD");
+// }
+
+// }
+
+// question no 3 
+
+// let userAge = document.querySelector("#input")
+// function yourAge() {
+// if(userAge.value >= 18){
+
+//     console.log("old enough");
+// } else {
+//    console.log("YOUNG ENOUGH");
+// }
+
+// }
+
+
+// question no 4 
+
+let personName = prompt('enter your name here')
+
+function onClickk(){
+
+if(personName === Khalid){
+    console.log("welcome khalid");
+} 
+
+}
